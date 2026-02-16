cc865b56b099c86df67e9b8951279a252c4685da
cehougis <ucfncho@ucl.ac.uk>
2026-02-16 13:21:04 +0800

    update google analysis

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,18 @@
 import "./globals.css";
+import { GoogleAnalytics } from '@next/third-parties/google'
 
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  return children;
+    return (
+    <html lang="en">
+      <body>{children}</body>
+      {/* 把组件放在这里，并填入你的 ID */}
+      <GoogleAnalytics gaId="G-G06J09XH2K" />
+    </html>
+  )
 }
+
+
